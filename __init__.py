4076c16a5ee94cf954944fa1b6773a8df4e8171d
ProJ-Yeet <100456997+ProJ-Yeet@users.noreply.github.com>
2026-08-08 17:00:27 +0545

    Version 1.3.1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 
 bl_info = {
-    "name" : "Medieval 2 Toolkit V1.3.0",
+    "name" : "Medieval 2 Toolkit V1.3.1",
     "author" : "WK",
     "description" : "Collection of tools and features for modders of Medieval 2: Total War",
     "blender" : (5, 0, 0),
-    "version" : (1, 3, 0),
+    "version" : (1, 3, 1),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
